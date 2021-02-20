9fe74e46c4be6f18a25a40ba94233bb39f7b89f7
박선웅 <@DESKTOP-4OPVN6T>
2021-02-20 15:10:37 +0900

    pom.xml 수정 root-context.xml 수정 notice관련 Controller, Service, Dao, xml 제작 PagingBean class 추가 박선웅

--- src/main/java/com/og/controllers/NoticeController.java
+++ src/main/java/com/og/controllers/NoticeController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.og.service.NoticeService;
-
+ 
 @Controller
 public class NoticeController {
 	@Autowired
